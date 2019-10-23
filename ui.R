8f7933a1d868b4d95cb5674a36654807aff21c00
Virginie Marques <45850493+virginiemarques@users.noreply.github.com>
2019-10-23 14:38:01 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ dashboardPage(
     fluidRow(
       
       column(width = 12,align="center",
-             tags$footer(tags$p("Click on a polygon to display the list of species \n \n")),
+             #tags$footer(tags$p("Click on a polygon to display the list of species \n \n")),
              
              column(width=12, selectInput("taxon_chosen", 
                                           label = "Choose a taxon",
@@ -21,7 +21,7 @@ dashboardPage(
                     selected = "Marine fish"),
              
              column(width=12, align="center", uiOutput("control_resolution")),
-
+             
              hr(), 
              column(width=12, selectInput("marker_position", 
                                           label = "Choose a mitochondrial position",
@@ -40,10 +40,10 @@ dashboardPage(
              
              hr(),
              tags$a("Code in GitHub", href="https://github.com/virginiemarques/Gaps_shiny_quicktest", target="_blank"),
-             tags$footer(tags$p("This application is developped by V. Marques and supports the following paper: (Paper name & hyperlink)")),
+             tags$footer(tags$p("This shiny-app is developped by V. Marques and supports the following paper: ")),
              tags$a("Paper link", href="https://github.com/virginiemarques/Gaps_shiny_quicktest", target="_blank"),
              hr(), 
-             tags$footer(tags$p("Last updated in July 2019", class = "credit"))
+             tags$footer(tags$p("Last updated in October 2019", class = "credit"))
              
       ))
     
@@ -52,11 +52,12 @@ dashboardPage(
     
     fluidRow(
       #verbatimTextOutput("datasettt"),
-      column(width = 12, withSpinner(leafletOutput("map"), type=6),# Try to put a waiting sign
-             
-             hr()),
+      column(width = 12, withSpinner(leafletOutput("map"), type=6)),# Try to put a waiting sign,
       
-      column(width = 12, DT::dataTableOutput("tableau"))
+      column(width = 12,  textOutput("selected_txt")),
+      
+      column(width = 12, hr(),
+             DT::dataTableOutput("tableau"))
       
     ) # End of fluidRow
   ) # End of body
